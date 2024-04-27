45b6cb984c4d75136b49bd42c7293fefe8b2f770
Timothy Renteria <Timothyxrenteria@gmail.com>
2024-04-26 18:07:59 -0700

    finished

--- frontend/components/QuoteForm.js
+++ frontend/components/QuoteForm.js
@@ -1,5 +1,5 @@
 import React, { useReducer } from 'react'
-
+import { useCreateQuoteMutation } from '../state/quotesApi'
 const CHANGE_INPUT = 'CHANGE_INPUT'
 const RESET_FORM = 'RESET_FORM'
 
@@ -23,6 +23,7 @@ const reducer = (state, action) => {
 
 export default function TodoForm() {
   const [state, dispatch] = useReducer(reducer, initialState)
+  const [createQuote, { error: creationError }] = useCreateQuoteMutation()
   const onChange = ({ target: { name, value } }) => {
     dispatch({ type: CHANGE_INPUT, payload: { name, value } })
   }
@@ -31,12 +32,21 @@ export default function TodoForm() {
   }
   const onNewQuote = evt => {
     evt.preventDefault()
-    resetForm()
+    const { authorName, quoteText } = state
+    createQuote({ authorName, quoteText })
+      .unwrap()
+      .then(data => {
+        console.log(data);
+        resetForm()
+      })
+      .catch(err => {
+        console.log(err.data.message)
+      })
   }
 
   return (
     <form id="quoteForm" onSubmit={onNewQuote}>
-      <h3>New Quote Form</h3>
+      <h3>New Quote Form {creationError && creationError.data.message}</h3>
       <label><span>Author:</span>
         <input
           type='text'
--- frontend/components/Quotes.js
+++ frontend/components/Quotes.js
@@ -4,34 +4,29 @@ import {
   setHighlightedQuote,
   toggleVisibility,
 } from '../state/quotesSlice'
+import {
+  useGetQuotesQuery,
+  useToggleFakeMutation,
+  useDeleteQuoteMutation
+} from '../state/quotesApi'
+
 
 export default function Quotes() {
-  const quotes = [
-    {
-      id: 1,
-      quoteText: "Don't cry because it's over, smile because it happened.",
-      authorName: "Dr. Seuss",
-      apocryphal: true,
-    },
-    {
-      id: 2,
-      quoteText: "So many books, so little time.",
-      authorName: "Frank Zappa",
-      apocryphal: false,
-    },
-    {
-      id: 3,
-      quoteText: "Be yourself; everyone else is already taken.",
-      authorName: "Oscar Wilde",
-      apocryphal: false,
-    },
-  ]
+  const { data: quotes, isLoading: gettingQuotes, isFetching: refreshingQuotes } = useGetQuotesQuery()
+  const [toggleFake, {isLoading: togglingQuote}] = useToggleFakeMutation()
+  const [deleteQuote, {isLoading: deletingQuote}] = useDeleteQuoteMutation()
   const displayAllQuotes = useSelector(st => st.quotesState.displayAllQuotes)
   const highlightedQuote = useSelector(st => st.quotesState.highlightedQuote)
   const dispatch = useDispatch()
   return (
     <div id="quotes">
-      <h3>Quotes</h3>
+      <h3>
+      {(gettingQuotes || refreshingQuotes) && 'Loading '}
+        Quotes 
+        {deletingQuote && ' deleting...'}
+        {togglingQuote && ' toggling...'}
+        
+      </h3>
       <div>
         {
           quotes?.filter(qt => {
@@ -45,9 +40,9 @@ export default function Quotes() {
                 <div>{qt.quoteText}</div>
                 <div>{qt.authorName}</div>
                 <div className="quote-buttons">
-                  <button>DELETE</button>
+                  <button onClick={() => deleteQuote(qt.id)}>DELETE</button>
                   <button onClick={() => dispatch(setHighlightedQuote(qt.id))}>HIGHLIGHT</button>
-                  <button>FAKE</button>
+                  <button onClick={() => toggleFake({ id: qt.id, quote: { apocryphal: !qt.apocryphal } })}>FAKE</button>
                 </div>
               </div>
             ))
